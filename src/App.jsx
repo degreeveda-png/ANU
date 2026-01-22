035dea899ba786ce9ba9e59249aed4cdfb58a9ec
degreeveda-png <degreeveda@gmail.com>
2026-01-22 10:13:13 +0530

    Add welcome heading to App component

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>hello</h2>
+      <h3>welcome</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
